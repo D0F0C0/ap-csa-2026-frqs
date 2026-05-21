aff66fac649e5281118ae8facf88d43b464495b8
D0F0C0 <jiangj10086@gmail.com>
2026-05-21 11:41:10 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,13 @@
+// part A
 
+public Account(String requestedName){
+
+    int i = 1;
+    username = requestedName;
+
+    while(!isAvailable(username)){
+
+        username = requestedName + i;
+        i++;
+    }
+}
